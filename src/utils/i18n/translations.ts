2897094a1c7b745d6da1b55eef918a18d68370fb
Patrick Mukherjee <patrick@mukherjee.de>
2023-04-18 03:20:47 +0200

    i18ns the index page!

---
+++ src/utils/i18n/translations.ts
@@ -0,0 +1,20 @@
+export const languages = {
+    en: 'English',
+    de: 'Deutsch',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+    en: {
+        'nav.home': 'Home',
+        'nav.about': 'About',
+        'nav.twitter': 'Twitter',
+        'index.latest_posts_title': 'Some translated thoughts ...'
+    },
+    de: {
+        'nav.home': 'Hauptseite',
+        'nav.about': 'über',
+        'index.latest_posts_title': 'Ein paar Gedanken ...'
+    },
+} as const;

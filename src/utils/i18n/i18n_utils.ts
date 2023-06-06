18649e31cab8540052d57ae10dab5bdadf0059d0
Patrick Mukherjee <patrick@mukherjee.de>
2023-06-06 08:08:45 +0200

    exchanges language code in urls

--- src/utils/i18n/i18n_utils.ts
+++ src/utils/i18n/i18n_utils.ts
@@ -17,20 +17,57 @@ export function useTranslations(lang: keyof typeof languages) {
     }
 }
 
-function getPreferedLanguage() {
+function getPreferedLanguage(): keyof typeof languages {
     const supportedLanguages = Object.keys(languages);
     console.log(`supported languages: ${supportedLanguages}`);
     const languageCodes = navigator.languages || [navigator.language];
 
-    for (const code of languageCodes) {
-        const language = code.toLowerCase().split(/[_-]+/)[0];
+    for (const languageCode of languageCodes) {
+        const language = languageCode.toLowerCase().split(/[_-]+/)[0];
         if (supportedLanguages.includes(language)) {
             console.log(`user prefered supported language is: ${language}`);
-            return language;
+            return language as keyof typeof languages;
         }
     }
     console.log(`none of the user's languages are supported, defaulting to: ${defaultLang}`);
     return defaultLang;
+}
+
+// redirects to the language identifyer URL, e.g. http://www.mypa.ge/page.html -> http://www.mypa.ge/en/page.html
+export function get_i18n_url(url: URL, lang: keyof typeof languages): URL {
+    console.log(`get i18n version of ${url}`)
+
+
+    const url_path_parts = url.pathname.split(`/`);
+    // is there a language code in the url?
+    // `http://localhost:3001/en`.split(`/`); => [``,`en`]
+    const url_first_part = url_path_parts[1];
+    console.log(`first part is: ${url_first_part}`);
+    console.log(`url parts are: ${url_path_parts}`);
+    // is a lang?
+    const supportedLanguages = Object.keys(languages);
+
+    let redirect_url = url;
+    if (supportedLanguages.includes(url_first_part)) {
+        // replace it with the desired lang
+        url_path_parts[1] = lang;
+        redirect_url = new URL(url.origin + url_path_parts.join(`/`));
+        console.log(`redirect url switching lang only is ${redirect_url}`);
+        return redirect_url;
+        // at the root?
+    } else if (!url_first_part) {
+        redirect_url = new URL(url.origin + lang);
+        console.log(`redirect url for root is ${redirect_url}`);
+        return redirect_url;
+        // url without language code (if language code in the url is not supported, the URL won't know)
+    } else if (url_first_part) {
+        redirect_url = new URL(url.origin + `/` + lang + `/` + url_path_parts.join(`/`));
+        console.log(`redirect url adding lang only is ${redirect_url}`);
+        return redirect_url;
+    }
+
+    // something went wrong
+    return redirect_url;
 }
 
 // redirects to the language identifyer URL, e.g. http://www.mypa.ge/page.html -> http://www.mypa.ge/en/page.html
@@ -47,25 +84,20 @@ function getPreferedLanguage() {
 // < /astro>
 //
 export function redirect_to_i18n() {
-    const url = window.location;
+    const url = new URL(window.location.href);
 
-    console.log(`in ${url}`)
+    console.log(`redirecting from ${url}`)
 
-    const supportedLanguages = Object.keys(languages);
-    for (const languageCode of supportedLanguages) {
-        if (url.pathname.includes(languageCode)) {
-            console.log(`not redirecting, already i18n page`);
-            return url;
-        }
+    const lang = getPreferedLanguage();
+    // const supportedLanguages = Object.keys(languages);
+    // for (const languageCode of supportedLanguages) {
+    //     if (url.pathname.includes(languageCode)) {
+    //         console.log(`not redirecting, already i18n page`);
+    //         return url;
+    //     }
+    // }
+    const newURL = get_i18n_url(new URL(location.href), lang);
+    if (url != newURL) {
+        location.replace(newURL);
     }
-    let redirect_url;
-    // remove trailing '/'
-    if (url.pathname == "/") {
-        redirect_url = `${url.origin}/${getPreferedLanguage()}`
-    }
-    else {
-        redirect_url = `${url.origin}/${getPreferedLanguage()}${url.pathname}`;
-    }
-    console.log(`redirecting to ${redirect_url}`);
-    window.location.replace(redirect_url);
 }
